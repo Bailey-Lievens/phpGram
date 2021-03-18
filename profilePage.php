c271310bca667509b88c1a334289c37ff76b4b14
Bailey Lievens <baileylievens@hotmail.be>
2021-03-18 18:21:37 +0100

    Initial commit

---
+++ profilePage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+
+    <link rel="stylesheet" href="css/normalize.css"> 
+    <link rel="stylesheet" href="css/general.css"> 
+</head>
+<body>
+    <header>
+        <img src="https://via.placeholder.com/200x80?text=Logo+location+probably" alt="logo">
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="#">Discover</a> <!--Maybe show all posts from everyone?--> 
+            <a href="profilePage.php">My profile</a>
+            <a href="login.php">Logout</a>
+        </nav>
+    </header>
+
+    <h1>Sick profile page coming soon</h1>
+</body>
+</html>
