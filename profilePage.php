57ed6f2e1bc41f45abd0c67b708ebadb7aa6edee
ellenhiel <Wt42t37VdKoh>
2021-03-26 15:05:25 +0100

    hamburger menu doesn't work, style myProfile isn't correct, rest ok

--- profilePage.php
+++ profilePage.php
@@ -1,6 +1,6 @@
 <?php 
     include_once('isloggedin.inc.php');
-    ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,14 +18,8 @@
 </head>
 
 
-<header>
-        <img src="https://via.placeholder.com/200x80?text=Logo+location+probably" alt="logo">
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="#">Discover</a> <!--Maybe show all posts from everyone?--> 
-            <a href="profilePage.php">My profile</a>
-            <a href="login.php">Logout</a>
-        </nav>
+    <header>
+        <?php include_once('navigation.inc.php'); ?>
     </header>
 <body>
 
