83976b346302455c0ed3402c639e4d3679ba8790
ellenhiel <Wt42t37VdKoh>
2021-04-01 19:04:56 +0200

    homepage lay-out

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>Pengram</title>
 
     <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/general.css"> 
     <link rel="stylesheet" type="text/css" href="css/index.css" />
 
     <style>
@@ -18,5 +19,48 @@
 </head>
 <body>
     <?php include_once("navigation.inc.php")?>
+    <section class="post">
+        <header>
+            <img src="https://via.placeholder.com/150" alt="profilePicture">
+            <a href="#">username</a>
+            <p>10 minutes ago</p>
+            <a href="#">...</a>
+        </header>
+        <div>
+            <img src="https://via.placeholder.com/250" alt="postPicture">
+            <p>description picture</p>
+        </div>
+        <section>
+            <a href="#">
+                like
+            </a>
+            <a href="#">
+                react
+            </a>
+        </section>
+    </section>
+
+    <section class="post">
+        <header>
+            <img src="https://via.placeholder.com/150" alt="profilePicture">
+            <a href="#">username</a>
+            <p>10 minutes ago</p>
+            <a href="#">...</a>
+        </header>
+        <div>
+            <img src="https://via.placeholder.com/250" alt="postPicture">
+            <p>description picture</p>
+        </div>
+        <section>
+            <a href="#">
+                like
+            </a>
+            <a href="#">
+                react
+            </a>
+        </section>
+    </section>
+
+    <a href="#" class="loadMore">load more</a>
 </body>
 </html>
