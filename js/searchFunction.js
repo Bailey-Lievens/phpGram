5edb4dbd4b3d76adfc70ccfa6b3e4884d1abb5cf
Bailey Lievens <baileylievens@hotmail.be>
2021-04-06 13:52:54 +0200

    Added  css/html for search bar and search results

---
+++ js/searchFunction.js
@@ -0,0 +1,5 @@
+var searchBar = document.getElementById("searchInput");
+
+searchBar.addEventListener("click", function(e){
+    
+})
--- navigation.inc.php
+++ navigation.inc.php
@@ -1,6 +1,18 @@
 <link rel="stylesheet" type="text/css" href="css/navigation.css"/>
 <header>
-    <img id="logo" src="images/logo.png" alt="logo">
+    <div id="logoSearchWrapper">
+        <img id="logo" src="images/logo.png" alt="logo">
+        <form method="get">
+            <label id="searchLabel" for="q">Search here!</label>
+            <input placeholder="Search Pengram" type="search" id="searchInput" name="q">
+            <ul id="searchResultList">
+                <li> <img src="images/Bailey.jpg" alt="BaileyProfilePicture"> <span>Bailey Lievens</span></li>
+                <li> <img src="images/Amelie.jpg" alt="AmelieProfilePicture"> <span>Amelie Gosiau</span></li>
+                <li> <img src="images/Ellen.jpg" alt="EllenProfilePicture"> <span>Ellen Hiel</span></li>
+            </ul>
+            <input id="searchSubmit" type="submit">
+        </form>
+    </div>
     <nav>
         <div id="navWrapper">
             <a href="index.php">Home</a>
@@ -10,5 +22,6 @@
         </div>
         <a href="#" id="menuButton"><div class="menu"></div><div class="menu"></div></a>
     </nav>
-    <script src="js/SlideOutMenu.js"></script>
+    <script src="js/slideOutMenu.js"></script>
+    <script src="js/searchFunction.js"></script>
 </header> 
