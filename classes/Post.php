1ad1fb28c5ca851c586535cb9f8645ce97ec1fa3
Bailey Lievens <baileylievens@hotmail.be>
2021-04-24 21:53:59 +0200

    Fixed variable name

--- classes/Post.php
+++ classes/Post.php
@@ -134,8 +134,8 @@
         //If no amount is specified it returns 20
         public static function getPostsByTag($tag, $amount = 20){
             $conn = Database::getConnection();
-            $query = $conn->prepare("SELECT users.username,users.profile_picture, posts.description, posts.picture, posts.date FROM posts INNER JOIN users ON posts.user_id = users.id WHERE description like CONCAT( '%', :searchQ, '%') ORDER BY date DESC LIMIT ".$amount."");
-            $query->bindValue(":searchQ","#".$tag);
+            $query = $conn->prepare("SELECT users.username,users.profile_picture, posts.description, posts.picture, posts.date FROM posts INNER JOIN users ON posts.user_id = users.id WHERE description like CONCAT( '%', :tag, '%') ORDER BY date DESC LIMIT ".$amount."");
+            $query->bindValue(":tag","#".$tag);
             $query->execute();
             $posts = $query->fetchAll();
             return $posts;
