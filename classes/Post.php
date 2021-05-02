864e344c3702f716f3fa5bffc5ced8c59597e9a0
ellenhiel <elhi12@hotmail.com>
2021-05-02 20:54:31 +0200

    design delete button done

--- classes/Post.php
+++ classes/Post.php
@@ -157,7 +157,7 @@
         //Returns all posts posted by the given userId
         public static function getPostsById($userId, $amount = 20){
             $conn = Database::getConnection();
-            $query = $conn->prepare("SELECT picture FROM posts WHERE user_id = :userId ORDER BY posts.date DESC LIMIT ".$amount."");
+            $query = $conn->prepare("SELECT picture, id FROM posts WHERE user_id = :userId ORDER BY posts.date DESC LIMIT ".$amount."");
             $query->bindValue(":userId", $userId);
             $query->execute();
             $posts = $query->fetchAll();
